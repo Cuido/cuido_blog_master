230064ce56cf677c13eb1a9c8c1414b69ddd0795
Manmeet Tarun <manmeet.tarun@grofers.com>
2019-12-01 16:19:21 +0530

    added backend

---
+++ app.js
@@ -0,0 +1,32 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cors = require('cors');
+var indexRouter = require('./routes/index');
+var winston = require('./logger');
+require('./database/db');
+
+const port = 3000;
+
+
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile)
+app.set('view engine', 'html');
+app.use(logger('combined'));
+app.use(cors());
+app.use('/', indexRouter);
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: false
+}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(port, () => {
+    winston.info('Connected on port: ' + port);
+})
+
+module.exports = app;
---
+++ database/db.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+var logger = require('../logger');
+
+mongoose.connect('mongodb://127.0.0.1:27017/cuido_blog', { useNewUrlParser: true }, { useUnifiedTopology: true })
+    .catch(error => logger.error(error));
+
+module.exports = {
+    db: mongoose
+}
