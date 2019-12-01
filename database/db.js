fd5cd7a6598981d0b8f3e08f04c6ff516c4606dd
Manmeet Tarun <manmeet.tarun@grofers.com>
2019-12-01 16:30:08 +0530

    decoupling

--- database/db.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var logger = require('../logger');
-
-mongoose.connect('mongodb://127.0.0.1:27017/cuido_blog', { useNewUrlParser: true }, { useUnifiedTopology: true })
-    .catch(error => logger.error(error));
-
-module.exports = {
-    db: mongoose
-}
